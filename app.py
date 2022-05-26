e710f49c2ba5f3436cffc96f499a3143b77eb8dd
WilliamSwann <18268@wc.school.nz>
2022-05-26 15:15:18 +1200

    added couple commented some code

--- app.py
+++ app.py
@@ -37,45 +37,45 @@ def sidenav1(): #this function grabs all of the items from the product table and
 
 @app.route('/', methods=['GET', 'POST'])  #this function
 def render_homepage():
-    cata_list = sidenav1() #grabbing assets for sidenav
+    cata_list = sidenav1() #showing categorys
 
     return render_template('home.html', catagories=cata_list, logged_in=is_logged_in(), teacher=is_teacher())
     #rendering home.html and returning all of the variables needed in the html
 
 @app.route('/contact')
 def render_contact_page():
-    cata_list = sidenav1()
+    cata_list = sidenav1() #showing categorys
     return render_template('contact.html', catagories=cata_list, logged_in=is_logged_in(), teacher=is_teacher())
     # rendering contact.html and returning all of the variables needed in the html
 
 @app.route('/login', methods=["GET", "POST"])
 def render_login_page():
-    if is_logged_in():
+    if is_logged_in(): #when logged in it will redirect you to home if you input login page
         redirect('/')
-    cata_list = sidenav1()
-    if request.method == "POST":
-        email = request.form['email'].strip().lower()
+    cata_list = sidenav1() #showing categorys
+    if request.method == "POST":    #if the website recives a post request it will run the login code.
+        email = request.form['email'].strip().lower() #getting email and password and putting in correct form
         password = request.form['password'].strip()
 
-        query = """SELECT id, fname, password, teacher FROM customer WHERE email = ?"""
+        query = """SELECT id, fname, password, teacher FROM customer WHERE email = ?""" #sql query to grab data from customer databse
         con = create_connection(DB_NAME)
         cur = con.cursor()
         cur.execute(query, (email,))
-        user_data = cur.fetchall()
+        user_data = cur.fetchall() #putting data into list
         con.close()
 
-        try:
+        try:  #checking if data matches any in database
             userid = user_data[0][0]
             firstname = user_data[0][1]
             db_password = user_data[0][2]
             teacher = user_data[0][3]
         except IndexError:
-            return redirect("/login?error=Email+andor+password+incorrect")
+            return redirect("/login?error=Email+andor+password+incorrect") #if it doesnt it redirects to login page and puts text in bar
 
         if not bcrypt.check_password_hash(db_password, password):
-            return redirect(request.referrer + "?error=Email+andor+password+incorrect")
+            return redirect(request.referrer + "?error=Email+andor+password+incorrect") #if passwords do not match redirects to login page and puts text in bar
 
-        session['email'] = email
+        session['email'] = email  #sessioning useful information to use for logged in and teacher
         session['userid'] = userid
         session['firstname'] = firstname
         session['teacher'] = teacher
@@ -87,19 +87,19 @@ def render_login_page():
 
 @app.route('/signup', methods=['GET', 'POST'])
 def render_signup_page():
-    if is_logged_in():
+    if is_logged_in():  # when logged in it will redirect you to home if you input login page
         redirect('/')
-    cata_list = sidenav1()
-    if request.method == 'POST':
+    cata_list = sidenav1()  # showing categorys
+    if request.method == 'POST':  # if the website recives a post request it will run the signup code.
         print(request.form)
-        fname = request.form.get('fname').strip().title()
+        fname = request.form.get('fname').strip().title()  # getting signup information and putting in correct form
         lname = request.form.get('lname').strip().title()
         email = request.form.get('email').strip().lower()
         password = request.form.get('password')
         password2 = request.form.get('password2')
         teacher = request.form.get('teacher')
 
-        if password != password2:
+        if password != password2:  # validating data and ading parameters, and if data does not comply it wll refresh page with error
             return redirect('/signup?error=Passwords+dont+match')
 
         if len(password) < 8:
@@ -107,30 +107,30 @@ def render_signup_page():
 
         hashed_password = bcrypt.generate_password_hash(password)
         con = create_connection(DB_NAME)
-        query = "INSERT INTO customer(id, fname, lname, email, password, teacher) VALUES(NULL,?,?,?,?,?)"
+        query = "INSERT INTO customer(id, fname, lname, email, password, teacher) VALUES(NULL,?,?,?,?,?)"  # sql query to add data into the customer database
 
         cur = con.cursor()
         try:
-            cur.execute(query, (fname, lname, email, hashed_password, teacher))
+            cur.execute(query, (fname, lname, email, hashed_password, teacher)) #adding the data to customer database
         except sqlite3.IntegrityError:
-            return redirect('/signup?error=Email+already+in+use')
+            return redirect('/signup?error=Email+already+in+use')  # posting error if email is already used
 
         con.commit()
         con.close()
-        return redirect("/login")
+        return redirect("/login")  # redirect to login page
 
     return render_template('signup.html', catagories=cata_list, logged_in=is_logged_in(), teacher=is_teacher())
     # rendering login.html and returning all of the variables needed in the html
 
 @app.route("/logout")
-def logout():
+def logout():  # popping session key to log the user out of the website
     print(list(session.keys()))
     [session.pop(key) for key in list(session.keys())]
     print(list(session.keys()))
     return redirect('/?message=See+you+next+time!')
 
 
-def is_logged_in():
+def is_logged_in():  # checking session for email to determine if a user is logged in
     if session.get("email") is None:
         print("not logged in")
         return False
@@ -139,7 +139,7 @@ def is_logged_in():
         return True
 
 
-def is_teacher():
+def is_teacher():  # checking session for email to determine if the user is a teacher
     if session.get("teacher") == "on":
         print("is Teacher")
         return True
@@ -148,43 +148,44 @@ def is_teacher():
         return False
 
 
-@app.route("/catagories/<cata_id>", methods=["GET", "POST"])
+@app.route("/catagories/<cata_id>", methods=["GET", "POST"])  # displays the words in the category selected by the user
 def render_catagories(cata_id):
-    cata_list = sidenav1()
+    cata_list = sidenav1()  # showing categories
 
     con = create_connection(DB_NAME)
-    query = """SELECT name_id, name, english, cata_id, category, definition, level, image FROM product WHERE cata_id=? ORDER BY name, english"""
+    query = """SELECT name_id, name, english, cata_id, category, definition, level, image FROM product WHERE cata_id=? ORDER BY name, english"""  # looking for the words and word information in the category selected by the user
+
     cur = con.cursor()
     cur.execute(query, (cata_id,))
-    maori_list = cur.fetchall()
+    maori_list = cur.fetchall()  # putting the words into a list
 
     con = create_connection(DB_NAME)
-    query = """SELECT cata_id, catali FROM catalist WHERE cata_id=?"""
+    query = """SELECT cata_id, catali FROM catalist WHERE cata_id=?"""  # finding the category name and info
     cur = con.cursor()
     cur.execute(query, (cata_id,))
-    cata_names_list = cur.fetchall()
+    cata_names_list = cur.fetchall()  # putting names into list
     con.close()
 
     return render_template("catagories.html", cata_names_list=cata_names_list, catagories=cata_list, maori=maori_list,
                            logged_in=is_logged_in(), teacher=is_teacher())
     # rendering home.html and returning all of the variables needed in the html
 
-@app.route("/name/<name_id>", methods=["GET", "POST"])
+@app.route("/name/<name_id>", methods=["GET", "POST"]) # displays the word selected by the user
 def render_maoriword(name_id):
-    cata_list = sidenav1()
+    cata_list = sidenav1() # showing categories
 
     con = create_connection(DB_NAME)
-    query = """SELECT name_id, name, english, cata_id, category, definition, level, image, date_time,edited_by FROM product WHERE name_id=? ORDER BY name"""
+    query = """SELECT name_id, name, english, cata_id, category, definition, level, image, date_time,edited_by FROM product WHERE name_id=? ORDER BY name""" # looking for the word information selected by the user
     cur = con.cursor()
     cur.execute(query, (name_id,))
     maori_list = cur.fetchall()
 
-    query = """SELECT cata_id, catali FROM catalist WHERE cata_id=?"""
+    query = """SELECT cata_id, catali FROM catalist WHERE cata_id=?""" # finding the category name and info
     cur = con.cursor()
     cur.execute(query, (maori_list[0][3],))
-    cata_names_list = cur.fetchall()
+    cata_names_list = cur.fetchall() # putting names into list
 
-    if is_logged_in() and is_teacher():
+    if is_logged_in() and is_teacher(): # if the user is a teacher(admin) they are allowed to update the information of the word.
 
         if request.method == "POST" and is_logged_in() and is_teacher():
             name = request.form.get('name').strip().title()
